954dd711ed7f78965e9d8d0a22f3e3c61838e7b7
dharanim04 <dharani.m04@gmail.com>
2021-07-05 21:22:40 +1000

    added fun and carousel

---
+++ jsScripts/index.js
@@ -0,0 +1,105 @@
+
+
+const recipeManager =new RecipeManager(0);
+console.log(recipeManager);
+recipeManager.load();
+recipeManager.render();
+
+
+// Select the New Task Form
+const form = document.querySelector("#newrecipeform");
+
+
+// Add an 'onsubmit' event listener
+form.addEventListener("submit", (event) => {
+  // Select the inputs
+  let validateName = document.querySelector("#newrecipename");
+  let validateIngredients = document.querySelector("#newrecipeingredients");
+ 
+  let validationFail = 0;
+
+  // Prevent default action
+  event.preventDefault();
+
+  // Call this to clear all the form fields after the submission
+  const clearFormFields = () => {
+    validateName.value = "";
+    validateIngredients.value = "";
+    validateName.classList.remove("is-valid");
+    validateIngredients.classList.remove("is-valid");
+  };
+  // Form validation for Task Name Field for min length 2
+  if (validateName.value.length > 2) {
+    validateName.classList.add("is-valid");
+    validateName.classList.remove("is-invalid");
+  } else {
+    validateName.classList.add("is-invalid");
+    validateName.classList.remove("is-valid");
+    validationFail++;
+  }
+  // Form validation for Task Description Field for min length 8
+  if (validateIngredients.value.length > 8) {
+    validateIngredients.classList.add("is-valid");
+    validateIngredients.classList.remove("is-invalid");
+  } else {
+    validateIngredients.classList.add("is-invalid");
+    validateIngredients.classList.remove("is-valid");
+    validationFail++;
+  }
+  // If validation fails then function will not proceed further and
+  // will return. The value of validationFail will also needed to be
+  // reset to 0.
+  // ----------------------------------------------------------------------------------
+  if (validationFail > 0) {
+    validationFail = 0;
+    return;
+  } else {
+    // Push the valid input into our tasks array
+    recipeManager.addreipe(
+      validateName.value,
+      validateIngredients.value
+    );
+    clearFormFields();
+    recipeManager.save();
+    recipeManager.render();
+  }
+});
+
+
+const recipeList = document.querySelector("#recipelist");
+// Add an 'onclick' event listener to the Tasks List
+recipeList.addEventListener("click", (event) => {
+
+  // Check if a "Delete" button was clicked
+  if (event.target.classList.contains("delete-button")) {
+    // Get the parent Task
+    const parentTask =
+      event.target.parentElement.parentElement.parentElement;
+console.log(parentTask);
+    // Get the taskId of the parent Task.
+    const recipeId = Number(parentTask.dataset.recipeId);
+    console.log(parentTask.dataset);
+    console.log('recipe id');
+console.log(recipeId);
+console.log('recipe ingredients');
+console.log(parentTask.dataset.recipeIngredients);
+    // Delete the task
+    recipeManager.deleteTask(recipeId);
+
+    // Save the tasks to localStorage
+    recipeManager.save();
+
+    // Render the tasks
+    recipeManager.render();
+  }
+});
+
+const namecustomer=prompt('Enter your name');
+console.log('mesggagte');
+console.log(document.getElementById("idWelcomeMsg"));
+if(namecustomer !==null && namecustomer !==''){
+document.getElementById("idWelcomeMsg").innerHTML=`Welcome ${namecustomer} `;
+} else{
+  document.getElementById("idWelcomeMsg").innerHTML=`Welcome to pinch of Salt `;
+ 
+}
---
+++ jsScripts/recipeManager.js
@@ -0,0 +1,117 @@
+const createRecipeHtml = (id, name, ingredients) => {
+  const html = `<li class="card" data-recipe-id="${id}" style="min-width: 30vw">
+  <div class="card-body">
+    <h5 class="card-title">${name}</h5>
+    <p class="card-text">Ingredients:
+      ${ingredients}
+    </p>
+    <div class="card-footer row justify-content-end">
+        <button class="btn btn-outline-danger delete-button ">
+          Delete
+        </button>
+    </div>
+  </div>
+</li>`;
+  return html;
+};
+// Create the TaskManager class
+class RecipeManager {
+  constructor(curntRecipeId = 0) {
+    this.recipes = [];
+    this.curntRecipeId = curntRecipeId;
+  }
+
+  //addrecipes
+  addreipe(name, ingredients) {
+    // Create a recipe object that we will push to the list of recipes
+    const recipe = {
+      // Increment the current recipe Id for each new recipe
+      id: this.curntRecipeId++,
+      name: name,
+      ingredients: ingredients    
+    };
+    this.recipes.push(recipe);
+  }
+
+//load
+load() {
+  // Check if any recipes are saved in localStorage
+  if (localStorage.getItem("recipes")) {
+    // Get the JSON string of recipes in localStorage
+    const recipesJson = localStorage.getItem("recipes");
+
+    // Convert it to an array and store it in our TaskManager
+    this.recipes = JSON.parse(recipesJson);
+  }
+
+  // Check if the currentId is saved in localStorage
+  if (localStorage.getItem("curntRecipeId")) {
+    // Get the currentId string in localStorage
+    const curntRecipeId = localStorage.getItem("curntRecipeId");
+
+    // Convert the currentId to a number and store it in our TaskManager
+    this.curntRecipeId = Number(curntRecipeId);
+  }
+}
+
+  render() {
+    let recipeHtmlList = [];
+    // Loop over our recipes and create the html, storing it in the array
+    for (let i = 0; i < this.recipes.length; i++) {
+      // Get the current recipe in the loop
+      const recipe = this.recipes[i];
+      
+      // Create the recipe html
+      const recipeHtml = createRecipeHtml(
+        recipe.id,
+        recipe.name,
+        recipe.ingredients       
+      );
+      // Push it to the recipesHtmlList array
+      recipeHtmlList.push(recipeHtml);
+    }
+   
+    // Create the recipesHtml by joining each item in the recipesHtmlList
+    // with a new line in between each item.
+    const recipeHtml = recipeHtmlList.join("\n");
+
+    // Set the inner html of the recipesList on the page
+    const recipeList = document.getElementById("recipelist");
+    recipeList.innerHTML = recipeHtml;
+  }
+
+  save() {
+    // Create a JSON string of the recipes
+    const recipesJson = JSON.stringify(this.recipes);
+
+    // Store the JSON string in localStorage
+    localStorage.setItem("recipes", recipesJson);
+
+    // Convert the currentId to a string;
+    const curntRecipeId = String(this.curntRecipeId);
+
+    // Store the currentId in localStorage
+    localStorage.setItem("curntRecipeId", curntRecipeId);
+  }
+
+  deleteTask(recipeId) {
+    // Create an empty array and store it in a new variable, newTasks
+    const newTasks = [];
+
+    // Loop over the recipes
+    for (let i = 0; i < this.recipes.length; i++) {
+      // Get the current recipe in the loop
+      const recipe = this.recipes[i];
+
+      // Check if the recipe id is not the recipe id passed in as a parameter
+      if (recipe.id !== recipeId) {
+        // Push the recipe to the newTasks array
+        newTasks.push(recipe);
+      }
+    }
+
+    // Set this.recipes to newTasks
+    this.recipes = newTasks;
+  }
+  //end class
+}
