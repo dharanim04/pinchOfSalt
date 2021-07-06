9075a0595b0da839625869a6c4d76302481533c3
dharanim04 <dharani.m04@gmail.com>
2021-07-06 20:47:37 +1000

    animated the delete functionality and added default 2 lstvals to the class

--- jsScripts/index.js
+++ jsScripts/index.js
@@ -1,21 +1,16 @@
-
-
-const recipeManager =new RecipeManager(0);
-console.log(recipeManager);
+// initialise class 
+const recipeManager = new RecipeManager(2);
 recipeManager.load();
 recipeManager.render();
-
-
 // Select the New Task Form
 const form = document.querySelector("#newrecipeform");
-
 
 // Add an 'onsubmit' event listener
 form.addEventListener("submit", (event) => {
   // Select the inputs
   let validateName = document.querySelector("#newrecipename");
   let validateIngredients = document.querySelector("#newrecipeingredients");
- 
+
   let validationFail = 0;
 
   // Prevent default action
@@ -55,68 +50,59 @@ form.addEventListener("submit", (event) => {
     return;
   } else {
     // Push the valid input into our tasks array
-    recipeManager.addreipe(
-      validateName.value,
-      validateIngredients.value
-    );
+    recipeManager.addreipe(validateName.value, validateIngredients.value);
     clearFormFields();
     recipeManager.save();
     recipeManager.render();
   }
 });
 
-
-
-
 //delete action
 const recipeList = document.querySelector("#recipelist");
 // Add an 'onclick' event listener to the Tasks List
 recipeList.addEventListener("click", (event) => {
-
-
-
-     // Check if a "Delete" button was clicked
+  // Check if a "Delete" button was clicked
   if (event.target.classList.contains("delete-button")) {
-
     // Get the parent Task
- 
-  const parentTask =
-  event.target.parentElement.parentElement.parentElement;
-  console.log('parent task');
-console.log(parentTask);
 
+    const parentTask = event.target.parentElement.parentElement.parentElement;
+    console.log("parent task");
+    console.log(parentTask);
 
-// Get the taskId of the parent Task.
-const recipeId = Number(parentTask.dataset.recipeId);
-console.log(parentTask.dataset);
-console.log('recipe id');
-console.log(recipeId);
-let idlstItem= 'idlst'+ recipeId;
-let element = document.getElementById(idlstItem);
-console.log('element');
-console.log(element);
-element.classList.add('lstDeleteStyle');
+    // Get the taskId of the parent Task.
+    const recipeId = Number(parentTask.dataset.recipeId);
+    // console.log(parentTask.dataset);
+    console.log("recipe id");
+    console.log(recipeId);
+    //get element to add transition (div card)
+    let idlstItem = "idCard" + recipeId;
+    let element = document.getElementById(idlstItem);
+    // console.log("element");
+    // console.log(element);
+    //add transform style to the card
+    element.classList.add("lstDeleteStyle");
 
-    // Delete the task
-    recipeManager.deleteTask(recipeId);
-
-    // Save the tasks to localStorage
-    recipeManager.save();
-
-    // Render the tasks
-    recipeManager.render();
+    //delay the delete function to see the transition and transformation
+    setTimeout(function () {
+      // Delete the task
+      recipeManager.deleteTask(recipeId);
+      // Save the tasks to localStorage
+      recipeManager.save();
+      // Render the tasks
+      recipeManager.render();
+    }, 1200);
   }
-    
-  
- 
- 
 });
 
-const namecustomer=prompt('Enter your name');
-console.log('mesggagte');
-console.log(document.getElementById("idWelcomeMsg"));
-if(namecustomer !==null && namecustomer !==''){
-document.getElementById("idWelcomeMsg").innerHTML=`Welcome ${namecustomer} `;
-} else{
-  document.getElementById("idWelcomeMsg").innerHTML=`Welcome to pinch of Salt `;
+const namecustomer = prompt("Enter your name");
+// console.log("mesggagte");
+// console.log(document.getElementById("idWelcomeMsg"));
+if (namecustomer !== null && namecustomer !== "") {
+  document.getElementById(
+    "idWelcomeMsg"
+  ).innerHTML = `Welcome ${namecustomer}.dddd `;
+} else {
+  document.getElementById(
+    "idWelcomeMsg"
+  ).innerHTML = `Welcome to pinch of Salt `;
 }
--- jsScripts/recipeManager.js
+++ jsScripts/recipeManager.js
@@ -1,23 +1,55 @@
 const createRecipeHtml = (id, name, ingredients) => {
-  const html = `<li class="card styForm m-1" data-recipe-id="${id}" id="idlst${id}" style="min-width: 30vw">
-  <div class="card-body">
-    <h5 class="card-title">${name}</h5>
-    <p class="card-text">Ingredients:
-      ${ingredients}
-    </p>
-    <div class="card-footer row justify-content-end">
-        <button class="btn btn-outline-danger delete-button ">
-          Delete
+  //   const html = `<li class="card styForm m-1" data-recipe-id="${id}" id="idlst${id}" style="min-width: 30vw">
+  //   <div class="card-body">
+  //     <h5 class="card-title">${name}</h5>
+  //     <p class="card-text">Ingredients:
+  //       ${ingredients}
+  //     </p>
+  //     <div class="card-footer row justify-content-end">
+  //         <button class="btn btn-outline-danger delete-button ">
+  //           Delete
+  //         </button>
+  //     </div>
+  //   </div>
+  // </li>`;
+
+  const html = `
+<div class="card" id="idCard${id}" data-recipe-id="${id}" style="width: 30rem">
+    <div class="card-header" id="heading${id}">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapse${id}" aria-expanded="true" aria-controls="collapse${id}">
+        ${name}
         </button>
+      </h5>
+    </div>
+
+    <div id="collapse${id}" class="collapse" aria-labelledby="heading${id}" data-parent="#recipelist" >
+      <div class="card-body">
+      <p class="card-text">Ingredients:${ingredients}</p>
+      </div>
+      <div class="card-footer d-flex justify-content-end">
+      <button class="btn btn-outline-danger delete-button ">Delete</button>
+    </div>
     </div>
   </div>
-</li>`;
+`;
   return html;
 };
 // Create the TaskManager class
 class RecipeManager {
-  constructor(curntRecipeId = 0) {
-    this.recipes = [];
+  constructor(curntRecipeId = 2) {
+    this.recipes = [
+      {
+        id: 0,
+        name: "Cakes",
+        ingredients: "Sugar 1Cup, Flour 1Cup, Butter 1/4Cup, 1tsp Baking Powder, A dash of salt",
+      },
+      {
+        id: 1,
+        name: "Bread",
+        ingredients: "Flour 1Cup, Yeast 1tsp, Butter 2Tbsp, Sugar 2Tbsp, Salt 1tsp",
+      },
+    ];
     this.curntRecipeId = curntRecipeId;
   }
 
@@ -28,31 +60,31 @@ class RecipeManager {
       // Increment the current recipe Id for each new recipe
       id: this.curntRecipeId++,
       name: name,
-      ingredients: ingredients    
+      ingredients: ingredients,
     };
     this.recipes.push(recipe);
   }
 
-//load
-load() {
-  // Check if any recipes are saved in localStorage
-  if (localStorage.getItem("recipes")) {
-    // Get the JSON string of recipes in localStorage
-    const recipesJson = localStorage.getItem("recipes");
+  //load
+  load() {
+    // Check if any recipes are saved in localStorage
+    if (localStorage.getItem("recipes")) {
+      // Get the JSON string of recipes in localStorage
+      const recipesJson = localStorage.getItem("recipes");
 
-    // Convert it to an array and store it in our TaskManager
-    this.recipes = JSON.parse(recipesJson);
+      // Convert it to an array and store it in our TaskManager
+      this.recipes = JSON.parse(recipesJson);
+    }
+
+    // Check if the currentId is saved in localStorage
+    if (localStorage.getItem("curntRecipeId")) {
+      // Get the currentId string in localStorage
+      const curntRecipeId = localStorage.getItem("curntRecipeId");
+
+      // Convert the currentId to a number and store it in our TaskManager
+      this.curntRecipeId = Number(curntRecipeId);
+    }
   }
-
-  // Check if the currentId is saved in localStorage
-  if (localStorage.getItem("curntRecipeId")) {
-    // Get the currentId string in localStorage
-    const curntRecipeId = localStorage.getItem("curntRecipeId");
-
-    // Convert the currentId to a number and store it in our TaskManager
-    this.curntRecipeId = Number(curntRecipeId);
-  }
-}
 
   render() {
     let recipeHtmlList = [];
@@ -60,24 +92,27 @@ load() {
     for (let i = 0; i < this.recipes.length; i++) {
       // Get the current recipe in the loop
       const recipe = this.recipes[i];
-      
+
       // Create the recipe html
       const recipeHtml = createRecipeHtml(
         recipe.id,
         recipe.name,
-        recipe.ingredients       
+        recipe.ingredients
       );
       // Push it to the recipesHtmlList array
       recipeHtmlList.push(recipeHtml);
     }
-   
+
     // Create the recipesHtml by joining each item in the recipesHtmlList
     // with a new line in between each item.
     const recipeHtml = recipeHtmlList.join("\n");
 
     // Set the inner html of the recipesList on the page
     const recipeList = document.getElementById("recipelist");
+    console.log("in render");
+    // console.log(recipeList);
     recipeList.innerHTML = recipeHtml;
+    // console.log(recipeHtml);
   }
 
   save() {
@@ -97,7 +132,6 @@ load() {
   deleteTask(recipeId) {
     // Create an empty array and store it in a new variable, newTasks
     const newTasks = [];
-
     // Loop over the recipes
     for (let i = 0; i < this.recipes.length; i++) {
       // Get the current recipe in the loop
@@ -109,7 +143,6 @@ load() {
         newTasks.push(recipe);
       }
     }
-
     // Set this.recipes to newTasks
     this.recipes = newTasks;
   }
